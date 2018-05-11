8bcf67081124e1df738156bdba4b2196f742c961
Jonathan Chow <me@echo.sh>
2018-05-11 15:35:54 +0200

    Add action to add pool cards to deck

--- src/Pool.jsx
+++ src/Pool.jsx
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { filteredPoolCards } from './selector';
+import { addCardToDeck } from './state';
 import Card from './Card';
 import './Pool.css';
 import type { GlobalState } from './state';
@@ -10,13 +11,18 @@ import type { CardDataInstance } from './selector';
 
 type Props = {
   filteredPool: CardDataInstance[],
+  addCardToDeck: (name: string) => void,
 };
 
-const Pool = ({ filteredPool }: Props) =>
+const Pool = ({ filteredPool, addCardToDeck }: Props) =>
   <div className="Pool">
     Pool:
     <div className="Pool-cards">
-      {filteredPool.map(card => <Card key={card.instanceId} {...card.card} />)}
+      {filteredPool.map(card => (
+        <div key={card.instanceId} onClick={() => addCardToDeck(card.card.name)}>
+          <Card {...card.card} />
+        </div>
+      ))}
     </div>
   </div>
 ;
@@ -24,5 +30,8 @@ const Pool = ({ filteredPool }: Props) =>
 export default connect(
   (state: GlobalState) => ({
     filteredPool: filteredPoolCards(state),
+  }),
+  (dispatch) => ({
+    addCardToDeck: (name: string) => dispatch(addCardToDeck(name)),
   }),
 )(Pool);
